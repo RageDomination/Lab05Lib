72c556f1b4da73c32ad459ed1f9416fa2062d2cf
Владос царь / <154845500+RageDomination@users.noreply.github.com>
2025-04-24 20:50:53 +0300

    Добавьте файлы проекта.

---
+++ Lab05Lib/ClassesLib.cs
@@ -0,0 +1,19 @@
+﻿namespace Lab05Lib
+{
+    public class Airplane
+    {
+        public string Model { get; set; }
+        public string Manufacturer { get; set; }
+        public int Year { get; set; }
+        public int Capacity { get; set; }
+        public double FuelCapacity { get; set; }
+        public double FuelConsumptionPer100Km { get; set; }
+        public bool HasAutopilot { get; set; }
+        public bool IsMilitary { get; set; }
+
+        public double CalculateMaxDistance()
+        {
+            return FuelConsumptionPer100Km > 0 ? (FuelCapacity / FuelConsumptionPer100Km) * 100 : 0;
+        }
+    }
+}
